70636cf8430f8dd07bb3e44732601bde824e8b36
Norstar134 <norstar134@gmail.com>
2024-09-04 14:52:57 +0100

    first commit

---
+++ component/Table.tsx
@@ -0,0 +1,50 @@
+'use client'
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+
+
+const Table = () => {
+
+    const [file, setFile] = useState<File|null>(null)
+    const [jsonData, setJsonData] = useState("")
+    console.log([file])
+
+    const previewData = () => {
+        if (file){
+            const reader = new FileReader()
+            reader.onload = (e) => {
+                const data = e.target?.result
+                if (data){
+                    const workbook = XLSX.read(data, { type: "binary" })
+                    const sheetName = workbook.SheetNames[1]
+                    const workSheet = workbook.Sheets[sheetName]
+                    const json = XLSX.utils.sheet_to_json(workSheet)
+                    setJsonData(JSON.stringify(json, null, 2))
+                }
+            };
+            reader.readAsArrayBuffer(file)
+        }
+    }
+    return(
+        <div className="py-8 space-y-8">
+            <div className="flex items-center gap-8">
+                <div>
+                    {/*buttons*/}
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
+                    <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer 
+                    file:hover:bg-gray-500 file:cursor-pointer bg-gray-50 file:text-white focus:outline-none file:bg-gray-700 
+                    file:border-gray-600" 
+                    id="file_input" 
+                    type="file" 
+                    accept=".xls,.xlsx"
+                    onChange={(e) =>setFile(e.target.files?e.target.files[0] : null)}/>
+                </div>
+                <button className="py-2 px-6 rounded bg-slate-300 text-slate-900" onClick={previewData}>Preview Data</button>
+            </div>
+
+            <pre>{jsonData}</pre>
+        </div>
+    )
+}
+
+export default Table;
